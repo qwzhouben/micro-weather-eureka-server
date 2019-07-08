0a25ee500168333b91ee33a67dc803074fb3b355
qwzhouben <18075736517@163.com>
2019-07-08 13:27:25 +0800

    eureka server

---
+++ src/main/java/com/zben/cloud/Application.java
@@ -0,0 +1,19 @@
+package com.zben.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @DESC:
+ * @AUTHOR: jhon.zhou
+ * @DATE: 2019/7/8 0008 12:28
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
